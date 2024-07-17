1c14562a8ffe1a520d7ce909f8df19da0a1a4c1b
henNet <dmf.n123@gmail.com>
2024-07-17 20:53:11 -0300

    feat: Instaladas/Configuradas as dependências e requisitos para gerar o build da aplicação (executável)

--- main.js
+++ main.js
@@ -2,10 +2,11 @@ const { app, BrowserWindow, Menu } = require("electron");
 const reload = require("electron-reload");
 
 /* Permite que qualquer alteração no código já seja aplicada
-   no programa, sem precisar executar novamente */
-reload(__dirname, {
-  electron: require(`${__dirname}/node_modules/electron`),
-});
+   no programa, sem precisar executar novamente 
+   OBS: RETIRAR ESSAS LINHAS DO RELOAD NO BUILD */
+// reload(__dirname, {
+//   electron: require(`${__dirname}/node_modules/electron`),
+// });
 
 // Menu.setApplicationMenu(null);
 
@@ -19,7 +20,8 @@ function criarJanela() {
   const janela = new BrowserWindow(config);
 
   janela.loadFile("index.html");
-  janela.setIcon("image.png");
+  // janela.loadURL("https://www.youtube.com.br");
+  // janela.setIcon("image.png");
   janela.setMenuBarVisibility(false);
   janela.setTitle("Teste"); /* Apenas se nao tiver o html carregado */
   // janela.maximize(); /* Para a app abrir em tela cheia */
